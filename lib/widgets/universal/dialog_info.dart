4a8c59bb539996b5dadf4fbe6498a8c1851b31a1
Marcel Magbual <magbualmarcel@gmail.com>
2024-03-21 02:44:23 +0800

    Top up functionality

--- lib/widgets/universal/dialog_info.dart
+++ lib/widgets/universal/dialog_info.dart
@@ -52,11 +52,11 @@ class DialogInfo {
                     Text(
                       subText,
                       textAlign: TextAlign.center,
-                      style: const TextStyle(
+                      style: TextStyle(
                         color: ColorPalette.accentBlack,
-                        // overflow: subText.length >= 55
-                        //     ? TextOverflow.ellipsis
-                        //     : TextOverflow.visible,
+                        overflow: subText.length >= 100
+                            ? TextOverflow.ellipsis
+                            : TextOverflow.visible,
                         fontFamily: 'Montserrat',
                         fontWeight: FontWeight.w400,
                         fontSize: 11,
--- lib/widgets/universal/dialog_loading.dart
+++ lib/widgets/universal/dialog_loading.dart
@@ -3,47 +3,53 @@ import 'package:citefest/constants/colors.dart';
 
 class DialogLoading {
   final String subtext;
+  final bool? willPop;
 
-  DialogLoading({required this.subtext});
+  DialogLoading({required this.subtext, this.willPop});
 
   build(BuildContext context) {
     return showDialog(
         context: context,
         barrierDismissible: false,
         builder: (context) {
-          return Material(
-            color: Colors.transparent,
-            child: Center(
-              child: Container(
-                decoration: const BoxDecoration(
-                  color: ColorPalette.bgColor,
-                  borderRadius: BorderRadius.all(Radius.circular(10)),
-                ),
-                height: 200,
-                width: 200,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    const SizedBox(
-                      width: 50,
-                      height: 50,
-                      child: Center(
-                          child: CircularProgressIndicator(
-                        color: ColorPalette.primary,
-                      )),
-                    ),
-                    const SizedBox(height: 8),
-                    Text(
-                      subtext,
-                      style: const TextStyle(
-                        color: ColorPalette.accentBlack,
-                        fontFamily: 'Montserrat',
-                        fontWeight: FontWeight.w400,
-                        fontSize: 14,
+          return WillPopScope(
+            onWillPop: () async {
+              return willPop ?? true;
+            },
+            child: Material(
+              color: Colors.transparent,
+              child: Center(
+                child: Container(
+                  decoration: const BoxDecoration(
+                    color: ColorPalette.bgColor,
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                  ),
+                  height: 200,
+                  width: 200,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      const SizedBox(
+                        width: 50,
+                        height: 50,
+                        child: Center(
+                            child: CircularProgressIndicator(
+                          color: ColorPalette.primary,
+                        )),
                       ),
-                    ),
-                  ],
+                      const SizedBox(height: 8),
+                      Text(
+                        subtext,
+                        style: const TextStyle(
+                          color: ColorPalette.accentBlack,
+                          fontFamily: 'Montserrat',
+                          fontWeight: FontWeight.w400,
+                          fontSize: 14,
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
